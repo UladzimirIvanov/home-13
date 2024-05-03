0df00d8b80a1de505186a6ecaa07e97e2686b420
UladzimirIvanov <ivanouum@gmail.com>
2024-05-03 10:59:11 +0300

    Created login exception

--- src/Task1/LogAndPass.java
+++ src/Task1/LogAndPass.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 
 public class LogAndPass {
 
-    static boolean logPassCPass(String login, String password, String confirmPassword){
+    static boolean logPassCPass(String login, String password, String confirmPassword) {
 
+        if (login.length() > 20 || login.contains(" ")) {
+            try {
+                throw new WrongLoginException(login);
+            } catch (WrongLoginException e){
+                System.out.println(e);
+            }
+        }
 
         System.out.println(password);
         System.out.println(confirmPassword);
--- src/Task1/WrongLoginException.java
+++ src/Task1/WrongLoginException.java
@@ -1,10 +1,16 @@
 package Task1;
 
 public class WrongLoginException extends Exception{
+    private String login;
     public WrongLoginException() {
     }
 
-    public WrongLoginException(String message) {
-        super(message);
+    public WrongLoginException(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public String toString() {
+        return "Login " + login + " содержит пробелы или имеет более 20 символов";
     }
 }
